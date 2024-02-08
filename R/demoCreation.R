c4e6aa849c30b4c834c1b712b8d5a3a4029ad32e
maxwell-miller-geo <mmm683@nau.edu>
2024-02-08 16:12:18 -0700

    Added some data for testing

--- R/demoCreation.R
+++ R/demoCreation.R
@@ -8,10 +8,11 @@
 # test <- terra::rast(r"(C:\Thesis\Arid-Land-Hydrology\R\test_water_rast.tif)")
 # # Create 10 x 10 matrix example matrix
 # grid <- mapply(base::rep, 100:91, 10) # create matrix 100 - 91
-# grid[5,] <- grid2[5,] - 1  # Carve the center channel
+# grid[5,] <- grid[5,] - 1  # Carve the center channel
 # demo <- terra::rast(grid) # create temporary raster
 # values(test) <- values(demo) # assign values to real raster extent
 # # Save raster
 # outfile <- r"(C:\Thesis\Arid-Land-Hydrology\R\Example\DemoElements)"
-# terra::writeRaster(test, filename = file.path(outfile, "demo_dem.tif"), overwrite = T)
+# terra::writeRaster(test, filename = "./inst/extdata/demo_dem.tif", overwrite = T)
 
+# exampleDEM <- terra::rast(file.path(outfile, "demo_dem.tif"))
