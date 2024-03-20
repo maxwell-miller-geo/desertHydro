05688f5cf3a295b4dc83958ed9403a51b79c480b
Maxwell Miller <mmm683@nau.edu>
2024-03-20 09:14:59 -0700

    updates

--- R/OutlineModel.R
+++ R/OutlineModel.R
@@ -1,55 +1,55 @@
 # Singular model
-source("ALHM.R")
+#source("ALHM.R")
 
-date <- "2022-08-27"
-rainfall_method <-  "gauges"
-time_step <- .5
-time_seconds <- time_step * 60
-#base <- r"(C:\Thesis\Arid-Land-Hydrology\Data\Waterhole\Outputs\All\RainComparison)"
-base <- r"(Z:\Thesis\Arid-Land-Hydrology\Data)"
-#watershedbase <- r"(C:\Thesis\Arid-Land-Hydrology\R\Example)"
-watershedbase <- r"(Z:\Thesis\Arid-Land-Hydrology\R\Example)"
-ModelFolder <- file.path(base, paste0("runoff-", rainfall_method,"-watershedcarve-",time_seconds,"-", date))
-simulation_length <- 20
-impervious <- T
-WatershedElements <- file.path(watershedbase, "ExperimentalElements")
-#ModelFolder <- r"(C:\Thesis\Arid-Land-Hydrology\Data\Waterhole\Outputs\All\Mini\runoff2022-07-24)"
-mini = F
-store = T
-gif = F
-crop = T
-discharge = T
-impervious = T
-overwrite = T
-write = T
-restartModel = F
-land_cover_file = "land_cover_soils.shp"
-key = "MUKEY"
-library(profvis)
-out <- arid_model(ModelFolder,date,time_step,simulation_length,WatershedElements,mini,rainfall_method,store,gif,crop,discharge,impervious,overwrite,write,restartModel,land_cover_file,key)
-
-# Gif creation
-gifCreation(ModelFolder, out[[1]], rainfall_method, gif = T)
-
-# Points of Interest
-# Last layer
-# last <- length(names(out[[3]]))
-# plot(out[[3]][[last]], legend = T) # Velocity plot
-soil <- terra::rast(file.path(ModelFolder, "soilStorage.tif"))
-surface <- terra::rast(file.path(ModelFolder, "SurfaceStorage.tif"))
-velocity <- terra::rast(file.path(ModelFolder, "VelocityStorage.tif"))
-slope <- terra::rast(file.path(ModelFolder, "slope.tif"))
-SS <- terra::rast(file.path(ModelFolder, "model_soil_stack.tif"))
-mannings_n <- SS$mannings_n
-for(x in 1:(nlyr(surface)-1)){
-  terra:::add(mannings_n) <- SS$mannings_n
-}
-
-velocities <- ManningsWideChannelVelocity(mannings_n, surface, slope, length = 10)
-last <- length(names(surface))
-plot(surface[[last]])
-maxDepth <- terra::vect(file.path(ModelFolder, "max-depth.shp"))
-points(maxDepth, col = "red", )
-maxVel <- terra::vect(file.path(ModelFolder, "max-velocity.shp"))
-points(maxVel, col = "blue")
-gaugedLocations <- vect(file.path(WatershedElements, "gauges.shp"))
+# date <- "2022-08-27"
+# rainfall_method <-  "gauges"
+# time_step <- .5
+# time_seconds <- time_step * 60
+# #base <- r"(C:\Thesis\Arid-Land-Hydrology\Data\Waterhole\Outputs\All\RainComparison)"
+# base <- r"(Z:\Thesis\Arid-Land-Hydrology\Data)"
+# #watershedbase <- r"(C:\Thesis\Arid-Land-Hydrology\R\Example)"
+# watershedbase <- r"(Z:\Thesis\Arid-Land-Hydrology\R\Example)"
+# ModelFolder <- file.path(base, paste0("runoff-", rainfall_method,"-watershedcarve-",time_seconds,"-", date))
+# simulation_length <- 20
+# impervious <- T
+# WatershedElements <- file.path(watershedbase, "ExperimentalElements")
+# #ModelFolder <- r"(C:\Thesis\Arid-Land-Hydrology\Data\Waterhole\Outputs\All\Mini\runoff2022-07-24)"
+# mini = F
+# store = T
+# gif = F
+# crop = T
+# discharge = T
+# impervious = T
+# overwrite = T
+# write = T
+# restartModel = F
+# land_cover_file = "land_cover_soils.shp"
+# key = "MUKEY"
+# library(profvis)
+# out <- arid_model(ModelFolder,date,time_step,simulation_length,WatershedElements,mini,rainfall_method,store,gif,crop,discharge,impervious,overwrite,write,restartModel,land_cover_file,key)
+#
+# # Gif creation
+# gifCreation(ModelFolder, out[[1]], rainfall_method, gif = T)
+#
+# # Points of Interest
+# # Last layer
+# # last <- length(names(out[[3]]))
+# # plot(out[[3]][[last]], legend = T) # Velocity plot
+# soil <- terra::rast(file.path(ModelFolder, "soilStorage.tif"))
+# surface <- terra::rast(file.path(ModelFolder, "SurfaceStorage.tif"))
+# velocity <- terra::rast(file.path(ModelFolder, "VelocityStorage.tif"))
+# slope <- terra::rast(file.path(ModelFolder, "slope.tif"))
+# SS <- terra::rast(file.path(ModelFolder, "model_soil_stack.tif"))
+# mannings_n <- SS$mannings_n
+# for(x in 1:(nlyr(surface)-1)){
+#   terra:::add(mannings_n) <- SS$mannings_n
+# }
+#
+# velocities <- ManningsWideChannelVelocity(mannings_n, surface, slope, length = 10)
+# last <- length(names(surface))
+# plot(surface[[last]])
+# maxDepth <- terra::vect(file.path(ModelFolder, "max-depth.shp"))
+# points(maxDepth, col = "red", )
+# maxVel <- terra::vect(file.path(ModelFolder, "max-velocity.shp"))
+# points(maxVel, col = "blue")
+# gaugedLocations <- vect(file.path(WatershedElements, "gauges.shp"))
--- R/Regression_Analysis.R
+++ R/Regression_Analysis.R
@@ -1,52 +1,52 @@
-# Regression analysis - PCA
-#install.packages("FactoMineR")
-#install.packages("corrplot")
-library(corrplot)
-library(FactoMineR)
-library(tidyverse)
-#library(stats)
-# Function that combines two datasets by time 
-combineData <- function(xDF, yDF, write = F, jointype = "left"){
-  x <- read_csv(xDF)
-  y <- read_csv(yDF)
-  if(jointype == "left"){
-    combine <- left_join(x, y)
-    combine[is.na(combine)] <- 0
-  }
-  if(jointype == "right"){
-    combine <- right_join(x, y)
-    combine[is.na(combine)] <- 0
-  }
-  
-  # if(is.array(combine$temp_c)){
-  #   combine$temp_c <- approx(combine$temp_c)
-  # }
-  # Fill in missing discharges with zeros
-  
-  if(write){
-    filename <- paste0("rain-discharge-combined.csv")
-    write_csv(combine, filename)
-  }
-  return(combine)
-}
-# Test - load in example .csv files
-# Day test
-rainDay <- "rain-data-hour.csv" # in root directory
-streamDay <- "max-discharge-per-hour.csv" # in root dir.
-
-xDF <- rainDay
-yDF <- streamDay
-
-combinedDF <- combineData(xDF, yDF, write = F, jointype = "right") # input for regression analysis
-
-# Calculate the correlation of the data.frame
-
-# Filter out some columns
-dfFiltered <- combinedDF[,c(-1)]
-cor.df <- cor(dfFiltered)
-corrplot(cor.df, type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
-#ggsave("Correlation-plot.png", plot = p)
-
-# PCA Analysis
-day_PCA <- PCA(dfFiltered) # I guess remove the time variable
+# # Regression analysis - PCA
+# #install.packages("FactoMineR")
+# #install.packages("corrplot")
+# library(corrplot)
+# library(FactoMineR)
+# library(tidyverse)
+# #library(stats)
+# # Function that combines two datasets by time
+# combineData <- function(xDF, yDF, write = F, jointype = "left"){
+#   x <- read_csv(xDF)
+#   y <- read_csv(yDF)
+#   if(jointype == "left"){
+#     combine <- left_join(x, y)
+#     combine[is.na(combine)] <- 0
+#   }
+#   if(jointype == "right"){
+#     combine <- right_join(x, y)
+#     combine[is.na(combine)] <- 0
+#   }
+#
+#   # if(is.array(combine$temp_c)){
+#   #   combine$temp_c <- approx(combine$temp_c)
+#   # }
+#   # Fill in missing discharges with zeros
+#
+#   if(write){
+#     filename <- paste0("rain-discharge-combined.csv")
+#     write_csv(combine, filename)
+#   }
+#   return(combine)
+# }
+# # Test - load in example .csv files
+# # Day test
+# rainDay <- "rain-data-hour.csv" # in root directory
+# streamDay <- "max-discharge-per-hour.csv" # in root dir.
+#
+# xDF <- rainDay
+# yDF <- streamDay
+#
+# combinedDF <- combineData(xDF, yDF, write = F, jointype = "right") # input for regression analysis
+#
+# # Calculate the correlation of the data.frame
+#
+# # Filter out some columns
+# dfFiltered <- combinedDF[,c(-1)]
+# cor.df <- cor(dfFiltered)
+# corrplot(cor.df, type = "upper", order = "hclust", tl.col = "black", tl.srt = 45)
+# #ggsave("Correlation-plot.png", plot = p)
+#
+# # PCA Analysis
+# day_PCA <- PCA(dfFiltered) # I guess remove the time variable
 
